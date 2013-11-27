bc162cd54a7efaff3563ef350e3999587637b296
Roger Camargo <huogerac@gmail.com>
2013-11-27 22:20:36 +1000

    using floppy-forms and hide username field on signup page (registration)

--- djangotemplate/signup/forms.py
+++ djangotemplate/signup/forms.py
@@ -1,1 +1,41 @@
-# Create your forms here.
+from django.utils.translation import ugettext_lazy as _
+#from django import forms
+import floppyforms as forms
+from floppyforms.widgets import PasswordInput, HiddenInput, CheckboxInput, EmailInput #TextInput
+from django.contrib.auth.models import User
+
+from registration.forms import RegistrationFormTermsOfService
+
+class RegistrationSignupForm(RegistrationFormTermsOfService, forms.Form):
+
+    username = forms.CharField(widget=HiddenInput(), label='', required=False)
+    email = forms.EmailField(widget=EmailInput(attrs={'placeholder': 'Enter a valid email'}), label=_("E-mail"))
+    password1 = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'Password'}), label=_("Password"))
+    password2 = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'Confirm Password'}), label=_("Password (again)"))
+
+    tos = forms.BooleanField(widget=CheckboxInput,
+                             label=_(u'I have read and agree to the Terms of Service'),
+                             error_messages={'required': _("You must agree to the terms to register")})
+
+    def clean_email(self):
+        """
+        Validate that the supplied email address is unique for the
+        site.
+        
+        """
+        if User.objects.filter(email__iexact=self.cleaned_data['email']):
+            raise forms.ValidationError(_("This email address is already in use. Please supply a different email address."))
+        return self.cleaned_data['email']
+
+    def clean(self):
+        
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError(_("The two password fields didn't match."))
+
+        if 'email' in self.cleaned_data:
+            #TODO: make sure the username is unique
+            username = self.cleaned_data['email']
+            self.cleaned_data['username'] = username[:username.find('@')]
+
+        return self.cleaned_data
--- djangotemplate/signup/urls.py
+++ djangotemplate/signup/urls.py
@@ -1,9 +1,27 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
 
+from registration.backends.default.views import RegistrationView
+from registration.backends.default.views import ActivationView
+
+from signup.forms import RegistrationSignupForm
 
 urlpatterns = patterns('',
 	
-	#registration
-	url(r'^accounts/', include('registration.backends.default.urls')),
+
+	url(r'^accounts/activate/complete/$', TemplateView.as_view(template_name='registration/activation_complete.html'),
+                           name='registration_activation_complete'),
+	
+	url(r'^accounts/activate/(?P<activation_key>\w+)/$', ActivationView.as_view(),
+                           name='registration_activate'),
+
+	url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationSignupForm), 
+						   name='registration_register'),
+
+	url(r'^accounts/register/complete/$', TemplateView.as_view(template_name='registration/registration_complete.html'),
+                           name='registration_complete'),
+
+	url(r'^register/closed/$', TemplateView.as_view(template_name='registration/registration_closed.html'),
+                           name='registration_disallowed'),
 
 )
